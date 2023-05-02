b0075a7f5a0797b78185620e5969600727cacdec
Glitch (m-en) <none>
2023-05-02 02:44:14 +0000

    ❣️🧤 Updated with Glitch

---
+++ src/components/core_animation.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent("scale-on-mouseenter", {
+  schema: {
+    to: { default: "2.5 2.5 2.5", type: "vec3" },
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    this.el.addEventListener("mouseenter", function () {
+      el.object3D.scale.copy(data.to);
+    });
+  },
+
+  tick: function (time, delta) {
+    var rotation = this.el.getAttribute("rotation");
+
+    // Update the rotation based on the component's schema values
+    rotation.y += 0.3;
+
+    // Set the new rotation on the entity
+    this.el.setAttribute("rotation", rotation);
+  },
+});

a143ade471b127418312739cb79ff2bdee18012c
Glitch (m-en) <none>
2023-05-02 06:20:48 +0000

    🕷️🗃️ Updated with Glitch

--- src/components/core_animation.js
+++ src/components/core_animation.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent("scale-on-mouseenter", {
   schema: {
-    to: { default: "1.1 1.1 1.1", type: "vec3" },
+    to: { default: "20 20 20", type: "vec3" },
   },
 
   init: function () {
@@ -15,7 +15,7 @@ AFRAME.registerComponent("scale-on-mouseenter", {
     var rotation = this.el.getAttribute("rotation");
 
     // Update the rotation based on the component's schema values
-    rotation.y += 0.3;
+    // rotation.y += 0.3;
 
     // Set the new rotation on the entity
     this.el.setAttribute("rotation", rotation);
